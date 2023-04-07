016d4160f986fd506b43fee677e55bd4ec70b009
Ahmed Mohamed Abo Elsood <20912018100378@fci.zu.edu.eg>
2023-04-07 14:06:43 +0200

    first commit

--- Maps/AppDelegate.swift
+++ Maps/AppDelegate.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
+
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey("AIzaSyCJsK5wHkqr5BWWmZv3_qRSfgszHrDX114")
+        
         return true
     }
 
--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -6,13 +6,67 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: GMSMapView!
+    
+    let lat = 30.073050
+    let long = 31.227392
+    
+    let locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        map.delegate = self
+        let camera = GMSCameraPosition(latitude: lat, longitude: long, zoom: 14)
+        map.camera = camera
+        
+        //getMyLocation()
     }
+    func getMyLocation(){
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        map.isMyLocationEnabled = true
+        map.settings.myLocationButton = true
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let myLocation = locations.first?.coordinate
+        
+        let camera = GMSCameraPosition(target: myLocation! , zoom: 14)
+        map.animate(to:camera)
+    }
+    
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        map.clear()
+        var marker = GMSMarker(position: coordinate)
+        marker.map = map
+        let camera = GMSCameraPosition(target: coordinate, zoom: 14)
+        map.animate(to: camera)
+    }
+    
+    func mapView(_ mapView: GMSMapView, didLongPressAt coordinate: CLLocationCoordinate2D) {
+        map.clear()
+    }
+    
+    
+    func mapView(_ mapView: GMSMapView, didTapPOIWithPlaceID placeID: String, name: String, location: CLLocationCoordinate2D) {
+        map.clear()
+        let marker = GMSMarker(position: location)
+        marker.title = " \(name)"
+        marker.map = map 
+    }
+    
+    
 
 
 }
